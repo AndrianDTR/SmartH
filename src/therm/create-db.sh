7a0861d390ec36b8969187dddfc9532abcd98c22
Andrian Yablonskyy <andrian.yablonsky@gmail.com>
2013-01-09 23:34:48 +0200

    Prepare

---
+++ src/therm/create-db.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+rrdtool create hometemp.rrd --start N --step 60 \
+DS:temp:GAUGE:600:U:U \
+DS:heatsink:GAUGE:600:U:U \
+DS:state:GAUGE:600:U:U \
+RRA:AVERAGE:0.5:1:12 \
+RRA:AVERAGE:0.5:1:288 \
+RRA:AVERAGE:0.5:12:168 \
+RRA:AVERAGE:0.5:12:720 \
+RRA:AVERAGE:0.5:288:365
---
+++ src/therm/get-temp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+def loadSettings(file):
+	print "Load settings"
+
+def checkModules():
+	print "checkModules"
+
+def getSensorTemp(id):
+	print "getSensorTemp"
+
+def getSensorsList():
+	print "getSensorsList"
+
+def setHeaterState(state):
+	print "Set heater state"
+
+def main():
+	print "main"
+
+if __name__ == main:
+	main()
