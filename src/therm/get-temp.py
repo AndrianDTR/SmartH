5e90f42dc897844d5719e1534093ca4796cfce30
Andrian Yablonskyy <andrian.yablonsky@gmail.com>
2013-01-10 00:51:04 +0200

    Load w1 modules

--- src/therm/get-temp.py
+++ src/therm/get-temp.py
@@ -1,10 +1,59 @@
 #!/usr/bin/env python
 
-def loadSettings(file):
-	print "Load settings"
+import os
+#import 
 
-def checkModules():
+def loadSettings(file):
+	consts = {}
+	with open('consts.py', 'r') as const_file:
+		for line in const_file:
+			data = line.split('=')
+			
+			if len(data) > 1:
+				consts[data[0].strip()] = data[1].strip()
+
+	return consts
+
+def checkModules(modules):
 	print "checkModules"
+	missed = []
+	res = True
+	with open("/proc/modules") as loadedModules:
+		for module in modules:
+			if not module in loadedModules:
+				res = False
+				missed.append(module)
+	
+	return res, missed
+		
+def loadModule(module):
+	print "Load module", module
+	os.system("sudo modprobe {0}".format(module))
+	
+"""
+$output = "";
+$attempts = 0;
+while ($output !~ /YES/g && $attempts < 5)
+{
+        $output = `sudo cat /sys/bus/w1/devices/28-*/w1_slave 2>&1`;
+        if($output =~ /No such file or directory/)
+        {
+                print "Could not find DS18B20\n";
+                last;
+        }
+        elsif($output !~ /NO/g)
+        {
+                $output =~ /t=(\d+)/i;
+                $temp = ($is_celsius) ? ($1 / 1000) : ($1 / 1000) * 9/5 + 32;
+                $rrd = `/usr/bin/rrdtool update $dir/hometemp.rrd N:$temp:$outtemp`;
+        }
+ 
+        $attempts++;
+}
+ 
+#print "Inside temp: $temp\n";
+#print "Outside temp: $outtemp\n";
+"""
 
 def getSensorTemp(id):
 	print "getSensorTemp"
@@ -17,6 +66,11 @@ def setHeaterState(state):
 
 def main():
 	print "main"
+	res, missed = checkModules(['w1-gpio', 'w1-therm'])
+	if not res:
+		for module in missed:
+			loadModule(module)
 
-if __name__ == main:
+if __name__ == '__main__':
 	main()
+
